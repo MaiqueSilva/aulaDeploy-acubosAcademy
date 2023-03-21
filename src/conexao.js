fdc1aa65a9eb7f6edfc50b7744f1fd8306ea6af7
Maique Almeida da Silva <maique.almeida@hotmail.com>
2023-03-20 22:15:23 -0300

    conteudo adicionado

--- src/conexao.js
+++ src/conexao.js
@@ -18,11 +18,12 @@ const senha = process.env.SENHA
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : 'localhost',
-      port : 5432,
-      user : 'postgres',
-      password : senha,
-      database : 'market_cubos'
+      host : process.env.DB_HOST,
+      port : process.env.DB_PORT,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASS,
+      database : process.env.DB_DATABASE,
+      ssl: {rejectUnauthorized: false}
     }
   });
 
