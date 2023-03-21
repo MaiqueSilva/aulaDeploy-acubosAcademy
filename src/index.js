404e8bbc0e6572f7892bc130ae0d68967798bb06
Maique Almeida da Silva <maique.almeida@hotmail.com>
2023-03-20 21:15:48 -0300

    testando

--- src/index.js
+++ src/index.js
@@ -9,4 +9,8 @@ app.use(express.json());
 app.use(cors());
 app.use(rotas);
 
-app.listen(3000);
+const port = process.env.PORT || 3000
+
+app.listen(port,()=>{
+    console.log("Tudo certo por aqui")
+});
